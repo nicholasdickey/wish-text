a7ef5ac8e0204015daccd3766da9e2feb8d3b6e7
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2023-08-18 23:05:52 -0400

    Added colors to report

--- pages/rx.tsx
+++ pages/rx.tsx
@@ -65,7 +65,7 @@ const ReportItem= function(name:string,expanded:string,setExpanded:any,sessionid
                 {signature&&<div>Signature:{signature}</div>}
                 {greeting&&<div>Greeting:{greeting}</div>}
                
-                {image&&<img width={128} height={128} src={image} alt={"Image"}/>}
+                {image&&<img width={4*64} height={3*64} src={image} alt={"Image"}/>}
                  {metaimage&&<img width={64*8} height={64*3} src={metaimage} alt={"Image"}/>}
                 
 
@@ -75,13 +75,13 @@ const ReportItem= function(name:string,expanded:string,setExpanded:any,sessionid
         });
 
   
-    return <Accordion key={name} style={{ borderRadius: 14 }} sx={{ mt: 5 }} expanded={expanded == sessionid} onChange={()=>setExpanded(sessionid)}>
+    return <Accordion key={name} style={{ background:'#844',color:"white",  borderRadius: 14 }} sx={{ mt: 5 }} expanded={expanded == sessionid} onChange={()=>setExpanded(sessionid)}>
                 <AccordionSummary
                   expandIcon={<ExpandMoreIcon />}
                   aria-controls="panel4bh-content"
                   id="panel4bh-header"
                 >
-                  <Typography sx={{ width: '33%', flexShrink: 0 }}>{name}</Typography>
+                  <Typography sx={{ width: '33%', flexShrink: 0,color:items.length>1?"#afa":"888" }}>{name}</Typography>
                 </AccordionSummary>
                 <AccordionDetails style={{ borderRadius: 14 }}>
                   <Box sx={{ my: 4 }}>
@@ -91,228 +91,6 @@ const ReportItem= function(name:string,expanded:string,setExpanded:any,sessionid
               </Accordion>
 }
 
-const ModeSwitch = styled.div`
-  position:absolute;
-  right:200px;
-  top:20px;  
-  z-index:100; 
- // color:white; 
- 
-  @media (max-width: 700px) {
-    top:0px;
-    right:0px;
-    font-size:3rem;;
-    min-height: 120px;
-  }
-  `;
-
-const Create = styled.div`
-position:absolute;
-left:200px;
-top:20px;  
-z-index:100; 
-// color:white; 
-
-@media (max-width: 700px) {
-  top:0px;
-  left:0px;
-  font-size:3rem;;
-  min-height: 120px;
-}
-`;
-interface BackgroundMode {
-  colorDark: string;
-  colorLight: string;
-}
-interface WebShareProps {
-  color: string;
-}
-
-const WBLogo = styled.div`
-  margin-right:30px;
-`;
-
-const Copyright = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  flex-wrap: wrap;
-
-  color:grey;
-
-  `;
-const Sub = styled.div`
-  margin:2px;
-`;
-
-
-interface FooterProps {
-  darkmode: string;
-};
-const Footer = styled.div<FooterProps>`
-    padding:1rem 1rem;
-    width:100%;
-    color:rgb(44, 46, 56);
-    //background-color: ${({ darkmode }) => darkmode == "true" ? '#252330' : 'rgb(232, 236, 239)'};
-    @media (max-width:990px){
-        padding:2px;
-    }
-    `;
-const LineContainer = styled.div<FooterProps>`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding:8px;  
-    text-align: center;
-    color:#2d2b38;
-    background-color: ${({ darkmode }) => darkmode == "true" ? '#999' : 'rgb(232, 236, 239)'};
-  `;
-
-const BandContainer = styled.div<{ darktext?: string, background?: string }>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 4rem 2rem;
-  text-align: center;
-  color: ${({ darktext }) => (darktext == "true" ? '#fff' : '#2d2b38')};
-  background-color: ${({ darktext }) => (darktext == "true" ? '#2d2b38' : '#fff')};
-  background-image: ${({ background }) => background ? `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${background})` : null}; 
-  background-repeat: repeat;
-  background-size: 900px 491px;
-  min-height:450px;
-
-`;
-
-const Title = styled(Typography)`
-  margin-bottom: 1rem;
-  @media (max-width: 900px) {
-    font-size:2rem;
-  }
-`;
-
-const Subtitle = styled(Typography)`
-  margin-bottom: 2rem;
-  text-overflow: wrap;
-  padding:10px;
-  @media (max-width: 700px) {
-    font-size:1rem;;
-  }
-  
-`;
-
-const CTAButton = styled(Button)`
-  margin-top: 2rem;
-`;
-
-const FirstBandContainer = styled.h1`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  flex-wrap: wrap;
-  justify-content: center;
-  padding: 5rem 0;
-  text-align: center;
-  background: url('wide-candles.jpg') ; /* Replace with your image URL */
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/wide-candles.jpg'); /* Replace with your image URL */
-  font-weight:500;
-  color: #fff;
-  min-height: 380px;
-  background-repeat: repeat;
-
-  background-size: 900px 491px;
-  font-size:5rem;
-  font-size:4rem;
-  @media (max-width: 700px) {
-    font-size:3rem;
-  }
-  
-`;
-const SecondBandContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  padding: 5rem 2rem;
-  text-align: center;
-  background: url('wide-candles.jpg') ; /* Replace with your image URL */
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('wide-christmas-candles.jpg'); /* Replace with your image URL */
- 
-  color: #fff;
-  min-height: 380px;
-  background-repeat: repeat;
-
-  background-size: 900px 491px;
-  font-size:4rem;
-  @media (max-width: 700px) {
-    font-size:3rem;
-  }
-  
-`;
-const Wide = styled.div`
-   position:relative;
-   height:100px;
-   width:100%; 
-
-`;
-
-const ImageDemo = styled.div`
-//max-width: 100%;
-//padding:60px;
-position:relative;
-width:${1380 / 3}px;
-height:${777 / 3}px;
-  
-//height:fit-content;
-@media (max-width: 700px) {
-    width:${1380 / 5}px;
-    height:${777 / 5}px;
-  }
-margin-top:40px;
-`;
-
-
-const Body = styled.div`
-position:relative;
-  width:100%;
-  height:100%;
- //min-height:100vh;
-  padding-top:1vh;
-  
-`;
-const CardWrap = styled.div`
-
-//padding-top:40%;
-    display:flex;
-    flex-direction:column;
-    justify-content: center;
-    @media (max-width: 900px) {
-        padding-top:5px;
-    }
-
-`;
-const Wrap = styled.div`
-    //position:absolute;
-    //bottom:0px;
-    width:100%;
-  
-   
-    display: flex;
-   // height:100%;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-end;
-`
-const OuterWrapper = styled.div`
-    position:relative;
-    width:100%;
-    height:100%;
-    display:flex;
-    flex-direction:column;
-    justify-content: space-between;
-    `;
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['300', '400', '700'], style: ['normal', 'italic'] })
 let v = false;
@@ -329,7 +107,7 @@ const [expanded,setExpanded]=React.useState<string>("");
   for (const key in report){
     const item=report[key];
     const {sessionid,items}=item
-    itemsAll.push(ReportItem(`Session ${sessionid},items:${item.items.length}`,expanded,setExpanded,sessionid,item))
+    itemsAll.push(ReportItem(`${sessionid},items:${item.items.length};last:${items.length>0?items[0].name:''}`,expanded,setExpanded,sessionid,item))
   };
 
 
@@ -355,9 +133,9 @@ const [expanded,setExpanded]=React.useState<string>("");
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
             </Script>
-         
+         <div style={{padding:"20px",background:"#444",color:"white",position:"absolute",top:"0"}}>
           {itemsAll}
-
+         </div>   
         
         </main>
 
